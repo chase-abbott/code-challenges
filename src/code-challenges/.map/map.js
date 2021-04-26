04118e70c56f86ed3ff34b2634e474f5900f2f98
chase-abbott <cabbott93@gmail.com>
2021-04-26 16:18:45 -0700

    TDD stringUpNumbers

--- src/code-challenges/.map/map.js
+++ src/code-challenges/.map/map.js
@@ -2,3 +2,9 @@ export function doubleNumbers(array) {
     const newArray = array.map(item => item * 2)
     return newArray;
 }
+
+export function stringItUp(array) {
+    const newArray = array.map(item => String(item))
+
+    return newArray;
+}
--- src/code-challenges/.map/map.test.js
+++ src/code-challenges/.map/map.test.js
@@ -1,7 +1,13 @@
-import { doubleNumbers } from './map.js';
+import { doubleNumbers, stringItUp } from './map.js';
 
 test('doubles numbers', () => {
     const input = [2, 5, 100]; // arrange
     const output = doubleNumbers(input); // act
     expect(output).toEqual([4, 10, 200]); // assert
+});
+
+test('strings up numbers', () => {
+    const input = [2, 5, 100]; // arrange
+    const output = stringItUp(input); // act
+    expect(output).toEqual(["2", "5", "100"]); // assert
 });
