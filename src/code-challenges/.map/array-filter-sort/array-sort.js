9b31645331e587c9e85d1df53406522dc6f74a26
chase-abbott <cabbott93@gmail.com>
2021-04-27 15:05:52 -0700

    TDD greatest to least

--- src/code-challenges/.map/array-filter-sort/array-sort.js
+++ src/code-challenges/.map/array-filter-sort/array-sort.js
@@ -5,3 +5,11 @@ export function leastToGreatest(arr) {
         return 0;
     })
 }
+
+export function greatestToLeast(arr) {
+    return arr.sort((a, b) => {
+        if (a > b) return -1;
+        if (a < b) return 1;
+        return 0;
+    })
+}
