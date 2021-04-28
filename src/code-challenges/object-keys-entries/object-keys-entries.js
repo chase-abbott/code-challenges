71134f66e72c285564bae6d7fc8bc56c47f106f8
chase-abbott <cabbott93@gmail.com>
2021-04-28 10:27:02 -0700

    TDD sorted keys

--- src/code-challenges/object-keys-entries/object-keys-entries.js
+++ src/code-challenges/object-keys-entries/object-keys-entries.js
@@ -2,3 +2,8 @@ export function capitalizeObjectKeys(obj){
   const array = Object.keys(obj);
   return array.map(item => item.toUpperCase());
 }
+
+export function sortedKeys(obj){
+  const array = Object.keys(obj);
+  return array.sort((a, b) => a - b);
+}
