b0d37b21a2989176978397f8e46bff964a831ae6
Chase <cabbott93@gmail.com>
2021-05-07 09:52:07 -0700

    TDD wordsToCharList

--- src/code-challenges/array-methods/array-methods.js
+++ src/code-challenges/array-methods/array-methods.js
@@ -1,8 +1,21 @@
 export function howMuchPencil(str){
   const array = [];
+
   for (let i = -1; i < str.length; i++){
     array.push(str.slice((i + 1)));
   }
   
+  return array;
+}
+
+export function wordsToCharList(str){
+  let array = [];
+  if (str){
+    for (let i = 0; i < str.length; i++){
+      array.push(str[i]); 
+    }
+  } else {
+    array = [str];
+  }
   return array;
 }
