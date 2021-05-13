ce088a8487e50fd80d50921a75fe80e61eb2521c
Chase Abbott <cabbott93@gmail.com>
2021-05-13 07:55:51 -0700

    TDD sortChildren

---
+++ src/code-challenges/regex/regex.js
@@ -0,0 +1,7 @@
+export function sortByChildren(array) {
+    return array.sort((a, b) => {
+        if (a.children.length > b.children.length) return 1;
+        if (a.children.length < b.children.length) return -1;
+        return 0;
+    })
+}
